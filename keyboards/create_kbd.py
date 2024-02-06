3638c129ddda22dbe32c0b1cb16c79df039b4c5a
mmmGaya <gorbatovskaya-aya@yandex.ru>
2024-02-07 00:46:11 +0300

    add keyboard

---
+++ keyboards/create_kbd.py
@@ -0,0 +1,25 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from aiogram.types import InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+
+kb = [
+    [
+        KeyboardButton(text='/Накладная'),
+        KeyboardButton(text='/Претензия'),
+        KeyboardButton(text='/Менеджер')
+    ],
+]
+
+keyboard = ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Доступные действия"
+    )
+
+
+builder_pret = InlineKeyboardBuilder()
+builder_pret.add(InlineKeyboardButton(text='Ответить', callback_data='callmanager'))
+
+
+
+
