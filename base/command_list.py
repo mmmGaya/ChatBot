e7e54dcfdcebab060e9e81ff68485b62e3e7914b
mmmGaya <gorbatovskaya-aya@yandex.ru>
2024-02-07 10:30:38 +0300

    delete command_list

--- base/command_list.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import BotCommand
-
-command_user_list = [
-    BotCommand(command='Накладная', description='Создать накладную'),
-    BotCommand(command='Претензия', description='Написать претензию'),
-    BotCommand(command='Менеджер', description='Вызвать менеджера')
-]
